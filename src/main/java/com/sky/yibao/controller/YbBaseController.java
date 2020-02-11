d9f6348d29bb01a01fc79f60d1e24ed5568b1bda
shizhanwei <shizhanwei123@126.com>
2020-02-11 09:16:44 +0800

    医保接口代码封装 szw

--- src/main/java/com/sky/yibao/controller/YbBaseController.java
+++ src/main/java/com/sky/yibao/controller/YbBaseController.java
@@ -7,15 +7,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.lang.reflect.ParameterizedType;
+
 public abstract class YbBaseController<T> {
     private static YbBaseService ybBaseService = new YbServiceImplAdaptor();
     @Autowired
     protected T ybRealService;
 
 
+    protected Class<T> getClassT() {
+        return (Class <T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
+
+
     @PostMapping(value = "/create", produces = "application/json;charset=UTF-8")
     public Object create(@RequestBody String reqJson) {
         JSONObject param = JSONObject.parseObject(reqJson);
+        param.put("className",getClassT().getSimpleName());
         return ybBaseService.create(param);
     }
 
@@ -23,18 +32,21 @@ public abstract class YbBaseController<T> {
     @PostMapping(value = "/delete", produces = "application/json;charset=UTF-8")
     public Object delete(@RequestBody String reqJson) {
         JSONObject param = JSONObject.parseObject(reqJson);
+        param.put("className",getClassT().getSimpleName());
         return ybBaseService.delete(param);
     }
 
     @PostMapping(value = "/update", produces = "application/json;charset=UTF-8")
     public Object update(@RequestBody String reqJson) {
         JSONObject param = JSONObject.parseObject(reqJson);
+        param.put("className",getClassT().getSimpleName());
         return ybBaseService.update(param);
     }
 
     @PostMapping(value = "/read", produces = "application/json;charset=UTF-8")
     public Object read(@RequestBody String reqJson) {
         JSONObject param = JSONObject.parseObject(reqJson);
+        param.put("className",getClassT().getSimpleName());
         return ybBaseService.read(param);
     }
 
